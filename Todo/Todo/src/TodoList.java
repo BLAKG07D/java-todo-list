1e8b506251ea14815c7f8ca2d36059f92aad452a
BLAKG07D <blakg07d@gmail.com>
2023-05-29 21:40:47 -0500

    class todolist completed

---
+++ Todo/Todo/src/TodoList.java
@@ -0,0 +1,27 @@
+import java.util.Date;
+
+public class TodoList {
+    String topic;
+
+public TodoList(String topic) {
+
+}
+public void display(){
+
+}
+
+public static void assignTask(String taskname){
+
+}
+
+public static Date assignDeadline(Date dueDate){
+
+return dueDate;
+}
+
+public void createtask(){
+    assignDeadline(null);
+    assignTask(topic);
+}
+
+}
